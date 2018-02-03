fdb7168e1c8beb11540b746350f9b3a339747721
hosainshohan1 <hossain_shohan1@live.com>
2018-02-03 13:42:02 +0600

    :boom:

---
+++ important package.php
@@ -0,0 +1,11 @@
+#image upload to directorty
+http://image.intervention.io/
+
+#laravel charts
+https://erik.cat/projects/Charts/docs
+
+#pdf download
+https://github.com/barryvdh/laravel-dompdf
+
+#export excel
+https://github.com/Maatwebsite/Laravel-Excel
